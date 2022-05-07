092131d06edba9eebbdd7dd474f3d4799da57168
Karel <karelkarunia24@gmail.com>
2022-05-07 02:01:40 +0200

    added basic Game CRUD

--- src/main/java/com/example/warehouse/game/GameController.java
+++ src/main/java/com/example/warehouse/game/GameController.java
@@ -1,9 +1,7 @@
 package com.example.warehouse.game;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,7 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/game")
+@RequestMapping(path = "/api/v1/games")
 public class GameController {
     private final GameService gameService;
 
@@ -23,5 +21,22 @@ public class GameController {
     @GetMapping
     public List<Game> getGames() throws MalformedURLException {
         return gameService.getGames();
+    }
+
+    @PostMapping
+    public void registerNewGame(@RequestBody Game game){
+        gameService.addNewGame(game);
+    }
+
+    @DeleteMapping(path = "{gameId}")
+    public void deleteGame(@PathVariable("gameId") Long gameId){
+        gameService.deleteGame(gameId);
+    }
+
+    @PutMapping(path = "{gameId}")
+    public void updateGame(@PathVariable("gameId") Long gameId, @RequestParam(required = false) String name,
+                           @RequestParam(required = false) String publisher, @RequestParam(required = false) String genres,
+                           @RequestParam(required = false) int requiredAge){
+        gameService.updateGame(gameId, name, publisher, genres, requiredAge);
     }
 }
--- src/main/java/com/example/warehouse/game/GameService.java
+++ src/main/java/com/example/warehouse/game/GameService.java
@@ -1,20 +1,70 @@
 package com.example.warehouse.game;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class GameService {
 
-    public List<Game> getGames() throws MalformedURLException {
-        URL url = new URL("http://baeldung.com");
+    private final GameRepository gameRepository;
 
-        return List.of(new Game(1L, "Counter Strike", "publisher 1", "genre1; genre2", 0,
-                LocalDate.of(2001, 10, 20), "about game", "min req",
-                url, url));
+    @Autowired
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<Game> getGames() throws MalformedURLException {
+
+        return gameRepository.findAll();
+    }
+
+    public void addNewGame(Game game) {
+        Optional<Game> optionalGame = gameRepository.findGameByName(game.getName());
+        if(optionalGame.isPresent()){
+            throw new IllegalStateException("data with this game name already exists.");
+        }
+        gameRepository.save(game);
+    }
+
+    public void deleteGame(Long gameId) {
+        boolean exists = gameRepository.existsById(gameId);
+        if(!exists){
+            throw new IllegalStateException("Game with Id " + gameId + " doesn't exist");
+        }
+        gameRepository.deleteById(gameId);
+    }
+
+    @Transactional
+    public void updateGame(Long gameId, String name, String publisher, String genres, int requiredAge) {
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new IllegalStateException("game with id " + gameId + "does not exist"));
+
+        if(name != null && name.length() > 0 && !Objects.equals(game.getName(), name)){
+            Optional<Game> optionalGame = gameRepository.findGameByName(name);
+            if(optionalGame.isPresent()){
+                throw new IllegalStateException("data with name " + name + " already exists.");
+            }
+            game.setName(name);
+        }
+
+        if(publisher != null && publisher.length() > 0 && !Objects.equals(game.getPublisher(), publisher)){
+            game.setPublisher(publisher);
+        }
+
+        if(genres != null && genres.length() > 0 && !Objects.equals(game.getGenres(), genres)){
+            game.setGenres(genres);
+        }
+
+        if(requiredAge > -1 && game.getRequiredAge() != requiredAge){
+            game.setRequiredAge(requiredAge);
+        }
     }
 }
