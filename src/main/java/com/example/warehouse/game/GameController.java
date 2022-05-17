1b4305d0e601005cb8925057ee4905faff78a4ee
Karel <karelkarunia24@gmail.com>
2022-05-18 00:29:31 +0200

    added csv importer class and import API

--- src/main/java/com/example/warehouse/game/GameController.java
+++ src/main/java/com/example/warehouse/game/GameController.java
@@ -1,10 +1,14 @@
 package com.example.warehouse.game;
 
+import com.example.warehouse.helper.CSVHelper;
+import com.example.warehouse.message.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.net.MalformedURLException;
@@ -37,6 +41,24 @@ public class GameController {
     @PostMapping
     public void registerNewGame(@Valid @RequestBody Game game){
         gameService.addNewGame(game);
+    }
+
+    @PostMapping("/import")
+    public ResponseEntity<ResponseMessage> importFromCSV(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        if (CSVHelper.hasCSVFormat(file)) {
+            try {
+                gameService.importGamesFromCSV(file);
+                message = "Uploaded the file successfully: " + file.getOriginalFilename();
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+            } catch (Exception e) {
+                message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+                System.out.println(e);
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
+        }
+        message = "Please upload a csv file!";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
     }
 
     @DeleteMapping(path = "{gameId}")
